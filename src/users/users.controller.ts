0f67e1464ac9ef8d3caa24f885a7203a0a4fa415
Tanmaya <vibhorgupta2008@aol.in>
2023-09-25 04:33:50 +0530

    Use custom interceptor and decorator to set user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,8 +17,12 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ShowUserDto } from './dtos/show-user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Controller('users')
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private userService: UsersService,
@@ -35,9 +39,19 @@ export class UsersController {
     return session.color;
   }
 
+  // @Get('/whoami')
+  // whoAmI(@Session() session: any) {
+  //   return this.userService.findOne(session.userId);
+  // }
+
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.userService.findOne(session.userId);
+  whoAmI(@CurrentUser() user: User) {
+    return user;
+  }
+
+  @Post('/signout')
+  signoutUser(@Session() session: any) {
+    session.userId = null;
   }
 
   @Post('/signup')
