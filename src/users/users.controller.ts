aa36dc3a636489dc26d0ba0743f92fb8e99a6389
Tanmaya <vibhorgupta2008@aol.in>
2023-09-25 01:10:23 +0530

    Add session info in signin and signup routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -35,14 +35,23 @@ export class UsersController {
     return session.color;
   }
 
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this.userService.findOne(session.userId);
+  }
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('/signin')
-  signInUser(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signinUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
