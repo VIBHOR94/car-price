2931a02520a3bea478763d238da6f2668125f34b
Tanmaya <vibhorgupta2008@aol.in>
2023-09-30 23:38:58 +0530

    Add types to test cases

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 it('Can create an instance of auth service', async () => {
   // Create a fake user service
-  const fakeUserService = {
+  const fakeUserService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as User),
   };
 
   const module = await Test.createTestingModule({
@@ -18,6 +19,5 @@ it('Can create an instance of auth service', async () => {
   }).compile();
 
   const service = module.get(AuthService);
-
   expect(service).toBeDefined();
 });
