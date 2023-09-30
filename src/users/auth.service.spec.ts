828aa6362c6a42558d259578f3cdd2a38dab867c
Tanmaya <vibhorgupta2008@aol.in>
2023-09-30 23:48:02 +0530

    Add test case for auth service

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,21 +3,27 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-it('Can create an instance of auth service', async () => {
-  // Create a fake user service
-  const fakeUserService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+describe('AuthService', () => {
+  let service: AuthService;
+  beforeEach(async () => {
+    // Create a fake user service
+    const fakeUserService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      { provide: UsersService, useValue: fakeUserService },
-    ],
-  }).compile();
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: fakeUserService },
+      ],
+    }).compile();
 
-  const service = module.get(AuthService);
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('Can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });
